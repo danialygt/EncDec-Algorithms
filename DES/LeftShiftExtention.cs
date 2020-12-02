9f4a378aedb9601483c3a8adc4c88593d63f3360
Danialygt <Danialygt@DESKTOP-5B55USK>
2020-12-02 15:40:26 +0330

    impliment left shift extensions for int[]

---
+++ DES/LeftShiftExtention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DES
+{
+    public static class LeftShiftExtention
+    {
+        public static int[] LeftShift (this int[] data)
+        {
+            int first = data[0];
+            for (int i = 0; i < data.Length - 1 ; i++)
+            {
+                data[i] = data[i + 1];
+            }
+            data[data.Length - 1] = first;
+            return data;
+        }
+
+    }
+}
