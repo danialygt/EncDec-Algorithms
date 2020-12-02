70a726e874cf4fa98f3bdc66b776b2aeba6f01e0
Danialygt <Danialygt@DESKTOP-5B55USK>
2020-12-02 12:43:45 +0330

    add extentions for convert binary to string

---
+++ DES/ConvertExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DES
+{
+    public static class ConvertExtensions
+    {
+        public static string StringToBinaryString(this string data)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+        public static string BinaryStringToString(this string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+
+    }
+}
---
+++ DESTest/ConvertExtensionsTest.cs
@@ -0,0 +1,42 @@
+﻿using DES;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace DESTest
+{
+    public class ConvertExtensionsTest
+    {
+        [Theory]
+        [InlineData("A", "01000001")]
+        [InlineData("Hello", "0100100001100101011011000110110001101111")]
+        [InlineData("5164896561",
+            "00110101001100010011011000110100001110000011100100110110001101010011011000110001")]
+        [InlineData("sdjkl as$34%d0oioa os",
+            "011100110110010001101010011010110110110000100000011000010111001100100100001100110011010000100101011001000011000001101111011010010110111101100001001000000110111101110011")]
+        [InlineData("", "")]
+        public void StringToBinaryStringTest(string text, string binaryText)
+        {
+            Assert.Equal(binaryText, text.StringToBinaryString());
+        }
+
+
+
+        [Theory]
+        [InlineData("01000001", "A")]
+        [InlineData("0100100001100101011011000110110001101111", "Hello")]
+        [InlineData("00110101001100010011011000110100001110000011100100110110001101010011011000110001",
+            "5164896561")]
+        [InlineData("011100110110010001101010011010110110110000100000011000010111001100100100001100110011010000100101011001000011000001101111011010010110111101100001001000000110111101110011", 
+            "sdjkl as$34%d0oioa os")]
+        [InlineData("", "")]
+        public void BinaryStringToStringTest(string binaryText, string text)
+        {
+            Assert.Equal(text, binaryText.BinaryStringToString());
+        }
+
+
+    }
+}
