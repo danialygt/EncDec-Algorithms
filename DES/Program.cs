3f570600ddcb759edb62215e33f09d922f5a116a
Danialygt <Danialygt@DESKTOP-5B55USK>
2020-12-02 12:43:13 +0330

    Add projects

---
+++ DES/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace DES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
