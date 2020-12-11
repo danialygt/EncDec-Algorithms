45fadae231d21b170dbf9ef509dabd788536ae91
Danialygt <daniyalyaghouti@gmail.com>
2020-12-11 23:14:29 +0330

    impliment double des

---
+++ EncDec Algorithms/DoubleDESAlgorithm/DoubleDESMotor.cs
@@ -0,0 +1,35 @@
+﻿using Alghotithms.DESAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alghotithms.DoubleDESAlgorithm
+{
+    public class DoubleDESMotor
+    {
+        private DESMotor desMotor;
+
+        public DoubleDESMotor()
+        {
+            desMotor = new DESMotor();
+        }
+
+
+
+        public string Encrypt(string plainText, string key1, string key2) 
+        {
+            var cipherText1 = desMotor.Encrypt(plainText, key1);
+            var finalCipherText = desMotor.Encrypt(cipherText1, key2);
+
+            return finalCipherText; 
+        }
+        public string Decrypt(string cipherText, string key1, string  key2) 
+        {
+            var cipherText1 = desMotor.Decrypt(cipherText, key2);
+            var finalPlainText = desMotor.Decrypt(cipherText1, key1);
+
+            return finalPlainText;
+        }
+
+    }
+}
